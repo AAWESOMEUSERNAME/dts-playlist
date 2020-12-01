24f93249092dc4764a54417fffa782fd0396120f
gugugu <17621718276@163.com>
2020-12-01 23:27:17 +0800

    fix: 日志输出逻辑错误

--- dts-playlist-ui/src/main/java/com/gugu/dts/playlist/ui/parser/impl/ParserImpl.java
+++ dts-playlist-ui/src/main/java/com/gugu/dts/playlist/ui/parser/impl/ParserImpl.java
@@ -8,7 +8,6 @@ import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import java.io.File;
 import java.util.Optional;
@@ -34,7 +33,7 @@ public class ParserImpl implements IParser {
             String bpm = tag.getFirst(FieldKey.BPM);
             log.debug("parsing file BPM: {}", bpm);
             final Pattern pattern = Pattern.compile("^\\d+(\\.\\d+)*$");
-            if (pattern.matcher(bpm).find()) {
+            if (!pattern.matcher(bpm).find()) {
                 log.warn("--------- file BPM is illegal! ---------");
             }
             return Optional.of(new IMusicFile() {
@@ -78,20 +77,4 @@ public class ParserImpl implements IParser {
             return Optional.empty();
         }
     }
-
-
-//    public static void main(String[] args) throws TagException, ReadOnlyFileException, CannotReadException, InvalidAudioFrameException, IOException {
-//        File file = new File("D:\\Data\\music\\09-05. Hop Skip And Jump.mp3");
-//        AudioFile f = AudioFileIO.read(file);
-//        int trackLength = f.getAudioHeader().getTrackLength();
-//        System.out.println("length : " + trackLength);
-//        System.out.println("BPM: " + f.getTag().getFirst(FieldKey.BPM));
-//        System.out.println("artist:" + f.getTag().getFields(FieldKey.ARTIST));
-//        System.out.println("artists: " + f.getTag().getFields(FieldKey.ARTISTS));
-//        Iterator<TagField> fields = f.getTag().getFields();
-//        for (Iterator<TagField> it = fields; it.hasNext(); ) {
-//            TagField field = it.next();
-//            System.out.println("id: " + field.getId() + " value:" + field);
-//        }
-//    }
 }
